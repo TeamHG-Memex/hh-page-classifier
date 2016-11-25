bb18ec3ab86052abcb3f1e97c5e7f874e33b8443
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-11-25 16:36:52 +0300

    Move pretraining stuff stuff into pretraining module

---
+++ hh_page_clf/pretraining/extract_texts.py
@@ -0,0 +1,37 @@
+import argparse
+import gzip
+from functools import partial
+try:
+    import ujson as json
+except ImportError:
+    import json
+import multiprocessing
+
+import json_lines
+import html_text
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    arg = parser.add_argument
+    arg('input', help='In .jl.gz format with html in "text" field')
+    arg('html_field', help='Field name where html is stored')
+    arg('output', help='Output in jl.gz format with text in "text" field')
+    args = parser.parse_args()
+
+    with json_lines.open(args.input) as f, gzip.open(args.output, 'wt') as outf:
+        with multiprocessing.Pool() as pool:
+            for text_item in pool.imap_unordered(
+                    partial(text_worker, html_field=args.html_field), f):
+                outf.write(json.dumps(text_item))
+                outf.write('\n')
+
+
+def text_worker(item, html_field):
+    url = item.get('url')
+    html = item.get(html_field)
+    text = html_text.extract_text(html)
+    text_item = {'text': text}
+    if url is not None:
+        text_item['url'] = url
+    return text_item
--- hh_page_clf/pretraining/train_lda.py
+++ hh_page_clf/pretraining/train_lda.py
@@ -1,14 +1,6 @@
 import argparse
-import gzip
-from functools import partial
-try:
-    import ujson as json
-except ImportError:
-    import json
-import multiprocessing
 
 import json_lines
-import html_text
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.pipeline import make_pipeline
@@ -73,29 +65,3 @@ def main():
         max_features=args.max_features,
     )
     joblib.dump(pipe, args.output, compress=3)
-
-
-def extract_texts():
-    parser = argparse.ArgumentParser()
-    arg = parser.add_argument
-    arg('input', help='In .jl.gz format with html in "text" field')
-    arg('html_field', help='Field name where html is stored')
-    arg('output', help='Output in jl.gz format with text in "text" field')
-    args = parser.parse_args()
-
-    with json_lines.open(args.input) as f, gzip.open(args.output, 'wt') as outf:
-        with multiprocessing.Pool() as pool:
-            for text_item in pool.imap_unordered(
-                    partial(text_worker, html_field=args.html_field), f):
-                outf.write(json.dumps(text_item))
-                outf.write('\n')
-
-
-def text_worker(item, html_field):
-    url = item.get('url')
-    html = item.get(html_field)
-    text = html_text.extract_text(html)
-    text_item = {'text': text}
-    if url is not None:
-        text_item['url'] = url
-    return text_item
